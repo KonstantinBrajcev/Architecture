dbc6fde37c1e58d7c5882a2b6d7c7023e92d696a
Konstantin <kastett@mail.ru>
2025-08-06 20:24:44 +0300

    Lesson_8

---
+++ Lesson_8/Program/Program.java
@@ -0,0 +1,87 @@
+package Program;
+
+// import models.Table;
+import models.TableModel;
+// import models.Reservation;
+import presenters.BookingPresenter;
+// import presenters.ViewObserver;
+import presenters.Model;
+import presenters.View;
+import views.BookingView;
+
+// import java.util.ArrayList;
+import java.util.Date;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        View view = new BookingView();
+        Model model = new TableModel();
+
+        BookingPresenter bookingPresenter = new BookingPresenter(view, model);
+        bookingPresenter.showTables();
+
+        view.reservationTable(new Date(), 3, "Станислав");
+
+        // Вызов доработанного метода changeReservationTable
+        view.changeReservationTable(1001, new Date(), 4, "Станислав");
+    }
+
+    // // Интерфейс Model
+    // public interface Model {
+    // void changeReservationTable(int tableNumber, Date date, int numberOfPersons,
+    // String reservationName);
+    // }
+
+    // // Класс BookingPresenter (модифицированный)
+    // public static class BookingPresenter {
+    // private final View view;
+    // private final Model model;
+
+    // public BookingPresenter(View view, Model model) {
+    // this.view = view;
+    // this.model = model;
+    // }
+
+    // public void showTables() {
+    // // Код для отображения столиков на экране
+    // }
+    // }
+
+    // // Класс BookingView (модифицированный)
+    // public static class BookingView implements View {
+
+    // private Model model; // Добавим поле model
+
+    // // Конструктор для инициализации model
+    // public BookingView() {
+    // this.model = new TableModel();
+    // }
+
+    // @Override
+    // public void changeReservationTable(int tableNumber, Date date, int
+    // numberOfPersons, String reservationName) {
+    // model.changeReservationTable(tableNumber, date, numberOfPersons,
+    // reservationName);
+    // }
+    // }
+
+    // // Класс TableModel (модифицированный)
+    // public static class TableModel implements Model {
+    // private ArrayList<Table> tables = new ArrayList<>();
+
+    // @Override
+    // public void changeReservationTable(int tableNumber, Date date, int
+    // numberOfPersons, String reservationName) {
+    // for (Table table : tables) {
+    // if (table.getNumber() == tableNumber) {
+    // table.setDate(date);
+    // table.setNumberOfPersons(numberOfPersons);
+    // table.setReservationName(reservationName);
+    // break;
+    // }
+    // }
+    // }
+    // }
+}
---
+++ Lesson_8/models/TableModel.java
@@ -0,0 +1,49 @@
+package models;
+
+import presenters.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class TableModel implements Model {
+
+  private Collection<Table> tables;
+
+  public Collection<Table> loadTables() {
+    if (tables == null) {
+      tables = new ArrayList<>();
+
+      tables.add(new Table());
+      tables.add(new Table());
+      tables.add(new Table());
+      tables.add(new Table());
+      tables.add(new Table());
+    }
+
+    return tables;
+  }
+
+  public int reservationTable(Date reservationDate, int tableNo, String name) {
+    for (Table table : loadTables()) {
+      if (table.getNo() == tableNo) {
+        Reservation reservation = new Reservation(reservationDate, name);
+        table.getReservations().add(reservation);
+        return reservation.getId();
+      }
+    }
+    throw new RuntimeException("Некорректный номер столика.");
+  }
+
+  public int changeReservationTable(int oldReservationNo, Date reservationDate, int tableNo, String name) {
+    for (Table table : loadTables()) {
+      for (Reservation reservation : table.getReservations()) {
+        if (reservation.getId() == oldReservationNo) {
+          table.getReservations().remove(reservation);
+          return reservationTable(reservationDate, tableNo, name);
+        }
+      }
+    }
+    throw new RuntimeException("Некорректный номер брони.");
+  }
+}
