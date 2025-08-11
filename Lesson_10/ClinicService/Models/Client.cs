5d8db83445b2a1d9268c2f605837bdde7067b12f
Konstantin <kastett@mail.ru>
2025-08-11 15:54:05 +0300

    Add lesson_10

--- Lesson_10/ClinicService/Models/Client.cs
+++ Lesson_10/ClinicService/Models/Client.cs
@@ -1,14 +1,16 @@
-﻿namespace ClinicService.Models
+﻿#nullable enable
+
+namespace ClinicService.Models
 {
     public class Client
     {
         public int ClientId { get; set; }
 
-        public string Document { get; set; }
+        public string? Document { get; set; }
 
-        public string SurName { get; set; }
+        public string? SurName { get; set; }
 
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         public string? Patronymic { get; set; }
 
--- Lesson_10/ClinicService/Models/Requests/UpdateClientRequest.cs
+++ Lesson_10/ClinicService/Models/Requests/UpdateClientRequest.cs
@@ -1,14 +1,16 @@
-﻿namespace ClinicService.Models.Requests
+﻿#nullable enable
+
+namespace ClinicService.Models.Requests
 {
     public class UpdateClientRequest
     {
         public int ClientId { get; set; }
 
-        public string Document { get; set; }
+        public string? Document { get; set; }
 
-        public string SurName { get; set; }
+        public string? SurName { get; set; }
 
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         public string? Patronymic { get; set; }
 
